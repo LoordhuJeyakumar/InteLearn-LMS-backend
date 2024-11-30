87ea4a33f9ab09a249bce0fa821426abbff1f497
Loordhu Jeyakumar <loordhujeyakumar@gmail.com>
2024-11-30 08:02:07 +0530

    feat(data): add categories data file for storing category information

---
+++ data/categoriesData.js
@@ -0,0 +1,25 @@
+const categories = [
+  {
+    name: "Web Development",
+    description: "Courses related to building websites and web applications.",
+  },
+  {
+    name: "Data Science",
+    description: "Learn data analysis, machine learning, and big data tools.",
+  },
+  {
+    name: "Programming Languages",
+    description:
+      "Master popular programming languages like Python, Java, and C++.",
+  },
+  {
+    name: "Design",
+    description: "Courses on graphic design, UI/UX design, and more.",
+  },
+  {
+    name: "Artificial Intelligence",
+    description: "Deep learning, natural language processing, and AI tools.",
+  },
+];
+
+module.exports = categories;
